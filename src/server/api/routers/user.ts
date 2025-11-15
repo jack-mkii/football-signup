4a746edf0205116318e5986f016622f2337a49a1
Jack Flann <jflann8@gmail.com>
2025-11-15 20:04:08 +0000

    Add setAdmin user router function

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,12 +1,19 @@
+import z from "zod";
 import {
   createTRPCRouter,
   publicProcedure,
 } from "~/server/api/trpc";
-import { getAll } from "~/server/repository/userRepository";
+import { getAll, update } from "~/server/repository/userRepository";
 
 export const userRouter = createTRPCRouter({
   getAll: publicProcedure
     .query(async () => {
       return await getAll();
     }),
+  
+  setAdmin: publicProcedure
+    .input(z.object({ userId: z.string(), isAdmin: z.boolean() }))
+    .mutation(async (opts) => {
+      return await update({ id: opts.input.userId, isAdmin: opts.input.isAdmin });
+    })
 });
